84efa176cfe189dd6e39b99d573a648b3db7584a
Ryan Weaver <ryan@thatsquality.com>
2020-09-11 10:09:03 -0400

    entire UI flow

--- src/Controller/MagicLinkLoginController.php
+++ src/Controller/MagicLinkLoginController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use Psr\Container\ContainerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class MagicLinkLoginController extends AbstractController
@@ -11,8 +12,22 @@ class MagicLinkLoginController extends AbstractController
     /**
      * @Route("/login")
      */
-    public function requestMagicLink()
+    public function requestMagicLink(Request $request)
     {
+        if ($request->isMethod('POST')) {
+            $email = $request->request->get('email');
+
+            return $this->redirectToRoute('magic_link_check_email');
+        }
+
         return $this->render('magic_link/login.html.twig');
+    }
+
+    /**
+     * @Route("/login/check-email", name="magic_link_check_email")
+     */
+    public function magicLinkCheckEmail()
+    {
+        return $this->render('magic_link/check_email.html.twig');
     }
 }
