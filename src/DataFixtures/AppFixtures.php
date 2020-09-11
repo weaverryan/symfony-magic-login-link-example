2e75e6bdf67e9a4b2db2d2b86308a0d961c1562c
Ryan Weaver <ryan@thatsquality.com>
2020-09-11 11:32:51 -0400

    user fixture

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,6 +2,7 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -9,8 +10,10 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $user = new User();
+        $user->setEmail('weaverryan@gmail.com');
+
+        $manager->persist($user);
 
         $manager->flush();
     }
